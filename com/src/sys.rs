0e1e07d70ab03785462583fa1e8dccaaedeb41fd
Robbert van der Helm <mail@robbertvanderhelm.nl>
2022-01-29 22:37:06 +0100

    Make the vst3_com::sys constants cross platform
    This makes the query interface functions correct on Linux and macOS.

--- com/src/sys.rs
+++ com/src/sys.rs
@@ -8,6 +8,14 @@ use std::cmp::PartialEq;
 pub type HRESULT = i32;
 
 /// Equivalent of the [FAILED macro](https://docs.microsoft.com/en-us/windows/win32/api/winerror/nf-winerror-failed)
+#[cfg(not(target_os = "windows"))]
+#[allow(non_snake_case)]
+pub fn FAILED(result: HRESULT) -> bool {
+    // See `vst3_sys::base::funknown` for all return codes
+    result < 0 || result >= E_INVALIDARG
+}
+/// Equivalent of the [FAILED macro](https://docs.microsoft.com/en-us/windows/win32/api/winerror/nf-winerror-failed)
+#[cfg(target_os = "windows")]
 #[allow(non_snake_case)]
 pub fn FAILED(result: HRESULT) -> bool {
     result < 0
@@ -22,6 +30,20 @@ pub const S_OK: HRESULT = 0;
 pub const NOERROR: HRESULT = 0;
 /// False
 pub const S_FALSE: HRESULT = 1;
+
+/// Argument was invalid
+#[cfg(not(target_os = "windows"))]
+pub const E_INVALIDARG: HRESULT = 2;
+/// Argument was invalid
+#[cfg(target_os = "windows")]
+pub const E_INVALIDARG: HRESULT = -0x7FF8_FFA9;
+
+/// No interface found
+#[cfg(not(target_os = "windows"))]
+pub const E_NOINTERFACE: HRESULT = -1;
+/// No interface found
+#[cfg(target_os = "windows")]
+pub const E_NOINTERFACE: HRESULT = -0x7FFF_BFFE;
 
 /// A globally unique identifier
 #[repr(C)]
